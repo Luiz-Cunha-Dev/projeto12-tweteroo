4ac5dde4af8c95335f28b058fc3f5a71a88b24a1
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2022-11-04 15:40:14 -0300

    Requisitos obrigatórios finalizados

--- index.js
+++ index.js
@@ -3,24 +3,64 @@ import cors from "cors"
 
 const app = express();
 app.use(cors());
+app.use(express.json())
 
-let tweets = [
-    {
-        username: "bobesponja",
-            avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-          tweet: "eu amo o hub"
+
+let usuarios = [];
+
+let tweets = [];
+
+app.post("/sign-up", (req, res) => {
+    const {username, avatar} = req.body
+
+    if(!username || !avatar){
+        res.status(409).send("Preencha todos os campos para prosseguir!");
+        return;
     }
-]
 
+    let usuario = {
+        username,
+        avatar
+    }
+
+    usuarios.push(usuario)
+    res.send("OK")
+});
 
 app.get("/tweets", (req, res) => {
 
-    res.send(tweets)
-})
+    let tweetsRecentes;
 
-app.post("/sign-up", (req, res) => {
+    if(tweets.length < 11){
+        tweetsRecentes = tweets;
+    }else{
+        tweetsRecentes = tweets.slice(tweets.length-10, tweets.length);
+    }
+    
+
+    res.send(tweetsRecentes);
+});
+
+
+app.post("/tweets", (req, res) => {
+    const {username, tweet} = req.body;
+    if(!tweet){
+        res.status(409).send("Campo de tweet vazio");
+        return;
+    }else if(!username){
+        res.status(409).send("Usuario não encontrado");
+        return;
+    }
+
+    let publicacao = {
+        username,
+        avatar: usuarios[usuarios.length - 1].avatar,
+        tweet
+    }
+
+    tweets.push(publicacao) 
 
     res.send("OK")
-})
+});
 
 app.listen(5000, () => console.log(`App online na porta 5000`));
